2f9a445f90db31b268e861e516c80d78d8bfebe5
stavshimko <stavshimko@gmail.com>
2020-05-19 13:02:39 +0300

    Updates

---
+++ precache-manifest.16cb5d8c6918808ad7d58cfb4ed47c78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbbfa65984b98313b7a6307f0c4735a1",
+    "url": "/MyBlogReact/index.html"
+  },
+  {
+    "revision": "5e6f609c1c2d6a3a05e6",
+    "url": "/MyBlogReact/static/css/main.378b10ae.chunk.css"
+  },
+  {
+    "revision": "2a780e099a6fd8655af5",
+    "url": "/MyBlogReact/static/js/2.b6766c2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyBlogReact/static/js/2.b6766c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6f609c1c2d6a3a05e6",
+    "url": "/MyBlogReact/static/js/main.024425e9.chunk.js"
+  },
+  {
+    "revision": "337ac09f87bfe2728ced",
+    "url": "/MyBlogReact/static/js/runtime-main.2e7bc1fb.js"
+  },
+  {
+    "revision": "98288858b9459f95349c0c539e917040",
+    "url": "/MyBlogReact/static/media/emptyImg.98288858.png"
+  }
+]);
