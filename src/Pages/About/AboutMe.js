6fa7059f9ff09706a9d08bbe5dab5f7312356ce2
stavshimko <stavshimko@gmail.com>
2020-05-19 12:56:49 +0300

    fixed warning

--- src/Pages/About/AboutMe.js
+++ src/Pages/About/AboutMe.js
@@ -2,7 +2,7 @@ import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faInstagram} from "@fortawesome/free-brands-svg-icons";
 
-function AboutMe() {
+function AboutMe(props) {
     console.log('run About Me')
     return (
             <div className="about-me">
