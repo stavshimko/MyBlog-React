f4fd70d7ea1dba6d6ca3274dd699a28608b0aa5b
stavshimko <stavshimko@gmail.com>
2020-05-19 13:43:19 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -34,15 +34,15 @@ var posts = [
         img: '',
         published: '2 days ago',
         author: 'Stav Shimko'
-    },
-    {
-        id: 3,
-        title: 'Blog post #3',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        img: '',
-        published: '1 days ago',
-        author: 'Stav Shimko'
     }
+    // {
+    //     id: 3,
+    //     title: 'Blog post #3',
+    //     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //     img: '',
+    //     published: '1 days ago',
+    //     author: 'Stav Shimko'
+    // }
 ]
 
 export default class App extends React.Component{
